b867ea506979cac2343b556fc8546189b9ae8bfa
abhishek-singh0 <abhishek.programs@gmail.com>
2021-12-23 12:54:20 +0530

    Some Changes: Setting and Procfile

--- srlab/settings.py
+++ srlab/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-wgkb_qryagqxlb8l28l$ue=#z_0++-aoe137x9g(fpjvj1q$u7'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
